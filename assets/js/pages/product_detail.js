6448124a8758ffc6e22a46356a7f78e4a558fdd1
ngoanh1002 <116912494+ngoanh1002@users.noreply.github.com>
2023-09-22 20:46:04 +0700

    a

--- assets/js/pages/product_detail.js
+++ assets/js/pages/product_detail.js
@@ -1,9 +1,15 @@
-import { apiurl, endpoint, fetchdata, formatprice } from "../components/help.js";
+import {
+  apiurl,
+  endpoint,
+  fetchdata,
+  formatprice,
+} from "../components/help.js";
 
 let cart = {};
-  if(localStorage.getItem('cart-id')) cart = JSON.parse(localStorage.getItem('cart-id'));
-let section = document.createElement('section')
-section.classList.add('home_details')
+if (localStorage.getItem("cart-id"))
+  cart = JSON.parse(localStorage.getItem("cart-id"));
+let section = document.createElement("section");
+section.classList.add("home_details");
 section.innerHTML = `
 
 <div class="container">
@@ -12,17 +18,17 @@ section.innerHTML = `
 
 
 `;
-let main = document.querySelector("main")
-main.appendChild(section)
+let main = document.querySelector("main");
+main.appendChild(section);
 export async function handleclassactive(div, e) {
-  if (div.querySelector('.size span.active')) {
-    div.querySelector('.size span.active').classList.remove('active');
+  if (div.querySelector(".size span.active")) {
+    div.querySelector(".size span.active").classList.remove("active");
   }
-  e.currentTarget.classList.add('active');
+  e.currentTarget.classList.add("active");
 }
 
 export async function render(params) {
-  let {id, name, price, image, quantity, size} = params;
+  let { id, name, price, image, quantity, size } = params;
   let div = document.createElement("div");
   div.classList.add("products_details_page");
   let formattedPrice = await formatprice(price);
@@ -52,93 +58,80 @@ export async function render(params) {
  
 	`;
 
- for (let[k, v] of Object.entries(size)) {
-    let span = document.createElement('span');
+  for (let [k, v] of Object.entries(size)) {
+    let span = document.createElement("span");
     span.innerHTML = `${v}`;
-    span.setAttribute('data-size', v);
-    span.addEventListener('click', function (e) {
+    span.setAttribute("data-size", v);
+    span.addEventListener("click", function (e) {
       handleclassactive(div, e);
     });
-    div.querySelector('.size').appendChild(span);}
-    
-  
-    
-    let increase = div.querySelector(".increase");
-    let decrease = div.querySelector(".decrease");
-    let qty = div.querySelector(".input-qty");
-    let number = parseInt(qty.innerHTML)
-     
-     
-async function handlecplup(type) {
-        
-        if(type == 'decrease'){
-          decrease.addEventListener('click', function(){
-                number += 1;
-                qty.innerHTML = number;
-            });
-           
-        }
-    if(type == 'increase'){
-          increase.addEventListener('click', function(){
-            number -= 1;
-            qty.innerHTML = number;
-            if (number < 1) {
-              number = 1 ;
-              alert('Lỗi số lượng sản phẩm') 
-          }
-            });
-          }
-          
-        }
-        handlecplup('increase')
-        handlecplup('decrease')
-    
-    
-     
-        div.querySelector('.add').addEventListener('click', function(){
-          let size_active = div.querySelector('.size span.active');
-          let key = name;
-          if (!size_active) {
-            alert('bạn chưa chọn size');
-            return false;
-          }
-          if (cart[key]){
-              cart[key].quantity += number;
-              cart[key].totalprice = cart[key].quantity * cart[key].price;
-              localStorage.setItem('cart-id', JSON.stringify(cart));
-          }
-         
-          else{
-              cart[key] = {
-                  quantity : number, 
-                  name: name,
-                  price: price,
-                  size: size_active.getAttribute('data-size'),
-                  image:image,
-                  totalprice: price*number 
-              }
-          }
-          localStorage.setItem('cart-id', JSON.stringify(cart));
-          updatatotalquantity(cart)
-        });
-        
+    div.querySelector(".size").appendChild(span);
+  }
 
-        
-      
-  section.querySelector('.container').appendChild(div)
-    return div
+  let increase = div.querySelector(".increase");
+  let decrease = div.querySelector(".decrease");
+  let qty = div.querySelector(".input-qty");
+  let number = parseInt(qty.innerHTML);
+
+  async function handlecplup(type) {
+    if (type == "decrease") {
+      decrease.addEventListener("click", function () {
+        number += 1;
+        qty.innerHTML = number;
+      });
+    }
+    if (type == "increase") {
+      increase.addEventListener("click", function () {
+        number -= 1;
+        if (number < 1) {
+          number = 1;
+          alert("Lỗi số lượng sản phẩm");
+        }
+        qty.innerHTML = number;
+      });
+    }
+  }
+  handlecplup("increase");
+  handlecplup("decrease");
+
+  div.querySelector(".add").addEventListener("click", function () {
+    let size_active = div.querySelector(".size span.active");
+    let key = name;
+    if (!size_active) {
+      alert("bạn chưa chọn size");
+      return false;
+    }
+    if (cart[key]) {
+      cart[key].quantity += number;
+      cart[key].totalprice = cart[key].quantity * cart[key].price;
+      localStorage.setItem("cart-id", JSON.stringify(cart));
+    } else {
+      cart[key] = {
+        quantity: number,
+        name: name,
+        price: price,
+        size: size_active.getAttribute("data-size"),
+        image: image,
+        totalprice: price * number,
+      };
+    }
+    localStorage.setItem("cart-id", JSON.stringify(cart));
+    updatatotalquantity(cart);
+  });
+
+  section.querySelector(".container").appendChild(div);
+  return div;
 }
 
-
 export async function updatatotalquantity() {
-	let totalquantity = 0;
-	for (let [k, v] of Object.entries(cart)) {
-		totalquantity += v.quantity;
-	}
-	localStorage.setItem('totalquantity', JSON.stringify(totalquantity));
-	document.querySelector('.cartquantity').innerHTML = `${totalquantity}`
-  console.log(totalquantity)
-	if(totalquantity == 0) {
-		document.querySelector('.cartquantity').innerHTML = ``
-	}
+  let totalquantity = 0;
+  for (let [k, v] of Object.entries(cart)) {
+    totalquantity += v.quantity;
+  }
+  localStorage.setItem("totalquantity", JSON.stringify(totalquantity));
+  document.querySelector(".cartquantity").innerHTML = `${totalquantity}`;
+  console.log(totalquantity);
+  if (totalquantity == 0) {
+    document.querySelector(".cartquantity").innerHTML = ``;
+  }
 }
